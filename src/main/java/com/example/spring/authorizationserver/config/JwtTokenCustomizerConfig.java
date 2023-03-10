b2cd1bf3548978ab03e0debebedfbb1189e70edc
Andreas Falk <info@andreas-falk.de>
2023-03-10 23:09:08 +0100

    Fixes gh-3

--- src/main/java/com/example/spring/authorizationserver/config/JwtTokenCustomizerConfig.java
+++ src/main/java/com/example/spring/authorizationserver/config/JwtTokenCustomizerConfig.java
@@ -3,6 +3,7 @@ package com.example.spring.authorizationserver.config;
 import com.example.spring.authorizationserver.security.OidcUserInfoService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
@@ -15,11 +16,13 @@ public class JwtTokenCustomizerConfig {
     @Bean
     public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer(OidcUserInfoService userInfoService) {
         return (context) -> {
-            if (ID_TOKEN.equals(context.getTokenType().getValue()) || ACCESS_TOKEN.equals(context.getTokenType())) {
-                OidcUserInfo userInfo = userInfoService.loadUser(
-                        context.getPrincipal().getName());
-                context.getClaims().claims(claims ->
-                        claims.putAll(userInfo.getClaims()));
+            if (!AuthorizationGrantType.CLIENT_CREDENTIALS.equals(context.getAuthorizationGrantType())) {
+                if (ID_TOKEN.equals(context.getTokenType().getValue()) || ACCESS_TOKEN.equals(context.getTokenType())) {
+                    OidcUserInfo userInfo = userInfoService.loadUser(
+                            context.getPrincipal().getName());
+                    context.getClaims().claims(claims ->
+                            claims.putAll(userInfo.getClaims()));
+                }
             }
         };
     }
