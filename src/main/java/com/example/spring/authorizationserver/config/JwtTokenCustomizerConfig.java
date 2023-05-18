177c2417b5e522c009680536d49b884cc6ef7f4c
Andreas Falk <info@andreas-falk.de>
2023-05-18 08:00:17 +0200

    fine tune token settings

--- src/main/java/com/example/spring/authorizationserver/config/JwtTokenCustomizerConfig.java
+++ src/main/java/com/example/spring/authorizationserver/config/JwtTokenCustomizerConfig.java
@@ -22,6 +22,7 @@ public class JwtTokenCustomizerConfig {
                             context.getPrincipal().getName());
                     context.getClaims().claims(claims ->
                             claims.putAll(userInfo.getClaims()));
+                    context.getJwsHeader().type("jwt");
                 }
             }
         };
