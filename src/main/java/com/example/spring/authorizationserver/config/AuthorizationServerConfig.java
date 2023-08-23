e777d553f89e534f6c0e22a5a8c3b720819b4ef1
Andreas Falk <info@andreas-falk.de>
2023-08-23 10:01:33 +0100

    update to latest version

--- src/main/java/com/example/spring/authorizationserver/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/spring/authorizationserver/config/AuthorizationServerConfig.java
@@ -12,58 +12,65 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
-import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.oidc.OidcProviderConfiguration;
 import org.springframework.security.oauth2.server.authorization.oidc.authentication.OidcUserInfoAuthenticationContext;
 import org.springframework.security.oauth2.server.authorization.oidc.authentication.OidcUserInfoAuthenticationToken;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
-import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
-import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
-import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
-import java.time.Duration;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
-@Configuration(proxyBeanMethods = false)
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
 public class AuthorizationServerConfig {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthorizationServerConfig.class);
 
     @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Order(1)
     public SecurityFilterChain h2ConsoleSecurityFilterChain(HttpSecurity http) throws Exception {
         http.securityMatcher(PathRequest.toH2Console())
-                .csrf().disable()
-                .headers().frameOptions().disable()
-                .and()
-                .authorizeHttpRequests().anyRequest().permitAll();
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .authorizeHttpRequests(r -> r.anyRequest().permitAll());
+        return http.build();
+    }
+
+    @Order(2)
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated()
+                )
+                .formLogin(withDefaults());
         return http.build();
     }
 
@@ -71,172 +78,45 @@ public class AuthorizationServerConfig {
      * Security config for all authz server endpoints.
      */
     @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE + 1)
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
-        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
-                new OAuth2AuthorizationServerConfigurer();
-        RequestMatcher endpointsMatcher = authorizationServerConfigurer
-                .getEndpointsMatcher();
-
         Function<OidcUserInfoAuthenticationContext, OidcUserInfo> userInfoMapper = (context) -> {
             OidcUserInfoAuthenticationToken authentication = context.getAuthentication();
             JwtAuthenticationToken principal = (JwtAuthenticationToken) authentication.getPrincipal();
-
             return new OidcUserInfo(principal.getToken().getClaims());
         };
 
-        authorizationServerConfigurer
-                .oidc((oidc) -> oidc
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = new OAuth2AuthorizationServerConfigurer();
+        RequestMatcher endpointsMatcher = authorizationServerConfigurer.getEndpointsMatcher();
+        http.securityMatcher(endpointsMatcher).authorizeHttpRequests((authorize) -> {
+            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)authorize.anyRequest()).authenticated();
+        }).csrf((csrf) -> {
+            csrf.ignoringRequestMatchers(endpointsMatcher);
+        }).apply(authorizationServerConfigurer);
+        authorizationServerConfigurer.oidc(
+                o -> o
+                        .providerConfigurationEndpoint(Customizer.withDefaults())
+                        .clientRegistrationEndpoint(Customizer.withDefaults())
                         .userInfoEndpoint((userInfo) -> userInfo
                                 .userInfoMapper(userInfoMapper)
                         )
-                );
+        );	// Enable OpenID Connect 1.0
+
         http
-                .securityMatcher(endpointsMatcher)
-                .authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().authenticated()
+                .exceptionHandling((exceptions) -> exceptions
+                        .defaultAuthenticationEntryPointFor(
+                                new LoginUrlAuthenticationEntryPoint("/login"),
+                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                        )
                 )
-                .csrf(csrf -> csrf.ignoringRequestMatchers(endpointsMatcher))
-                .exceptionHandling(exceptions ->
-                        exceptions.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
-                )
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-                .apply(authorizationServerConfigurer);
+                .oauth2ResourceServer((resourceServer) -> resourceServer
+                        .jwt(Customizer.withDefaults()));
         return http.build();
     }
 
-    /*
-     * Repository with all registered OAuth/OIDC clients.
-     */
     @Bean
-    public RegisteredClientRepository registeredClientRepository(JdbcTemplate jdbcTemplate, PasswordEncoder passwordEncoder) {
-        Set<String> redirectUris = new HashSet<>();
-        redirectUris.add("http://127.0.0.1:9095/client/callback");
-        redirectUris.add("http://127.0.0.1:9095/client");
-        redirectUris.add("http://127.0.0.1:9090/login/oauth2/code/spring");
-        redirectUris.add("http://127.0.0.1:9095/client/login/oauth2/code/spring");
-        redirectUris.add("http://localhost:9095/client/callback");
-        redirectUris.add("http://localhost:9095/client");
-        redirectUris.add("http://localhost:9090/login/oauth2/code/spring");
-        redirectUris.add("http://localhost:9095/client/login/oauth2/code/spring");
-        redirectUris.add("https://oauth.pstmn.io/v1/callback");
-
-        RegisteredClient demoClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("demo-client")
-                .clientSecret(passwordEncoder.encode("secret"))
-                .clientAuthenticationMethods(methods -> methods.addAll(
-                        List.of(
-                                ClientAuthenticationMethod.CLIENT_SECRET_BASIC,
-                                ClientAuthenticationMethod.CLIENT_SECRET_POST,
-                                ClientAuthenticationMethod.NONE
-                        )))
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .tokenSettings(TokenSettings.builder().accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
-                        .accessTokenTimeToLive(Duration.ofMinutes(15))
-                        .authorizationCodeTimeToLive(Duration.ofMinutes(2)).build())
-                .redirectUris(uris -> {
-                    uris.addAll(redirectUris);
-                })
-                .scopes(scopes -> scopes.addAll(List.of(
-                        OidcScopes.OPENID, OidcScopes.PROFILE, OidcScopes.EMAIL, "offline_access"
-                )))
-                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(false).build())
-                .build();
-
-        RegisteredClient demoClientPkce = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("demo-client-pkce")
-                .clientAuthenticationMethods(methods -> methods.addAll(
-                        List.of(
-                                ClientAuthenticationMethod.CLIENT_SECRET_BASIC,
-                                ClientAuthenticationMethod.CLIENT_SECRET_POST,
-                                ClientAuthenticationMethod.NONE
-                        )))
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .tokenSettings(TokenSettings.builder().accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
-                        .accessTokenTimeToLive(Duration.ofMinutes(15))
-                        .authorizationCodeTimeToLive(Duration.ofMinutes(2)).build())
-                .redirectUris(uris -> {
-                    uris.addAll(redirectUris);
-                })
-                .scopes(scopes -> scopes.addAll(List.of(
-                        OidcScopes.OPENID, OidcScopes.PROFILE, OidcScopes.EMAIL, "offline_access"
-                )))
-                .clientSettings(ClientSettings.builder().requireProofKey(true).requireAuthorizationConsent(false).build())
-                .build();
-
-        RegisteredClient demoClientOpaque = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("demo-client-opaque")
-                .clientSecret(passwordEncoder.encode("secret"))
-                .clientAuthenticationMethods(methods -> methods.addAll(
-                        List.of(
-                                ClientAuthenticationMethod.CLIENT_SECRET_BASIC,
-                                ClientAuthenticationMethod.CLIENT_SECRET_POST,
-                                ClientAuthenticationMethod.NONE
-                        )))
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .tokenSettings(TokenSettings.builder().accessTokenFormat(OAuth2TokenFormat.REFERENCE)
-                        .accessTokenTimeToLive(Duration.ofMinutes(15))
-                        .authorizationCodeTimeToLive(Duration.ofMinutes(2)).build())
-                .redirectUris(uris -> {
-                    uris.addAll(redirectUris);
-                })
-                .scopes(scopes -> scopes.addAll(List.of(
-                        OidcScopes.OPENID, OidcScopes.PROFILE, OidcScopes.EMAIL, "offline_access"
-                )))
-                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(false).build())
-                .build();
-
-        RegisteredClient demoClientPkceOpaque = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("demo-client-pkce-opaque")
-                .clientSecret(passwordEncoder.encode("secret"))
-                .clientAuthenticationMethods(methods -> methods.addAll(
-                        List.of(
-                                ClientAuthenticationMethod.CLIENT_SECRET_BASIC,
-                                ClientAuthenticationMethod.CLIENT_SECRET_POST,
-                                ClientAuthenticationMethod.NONE
-                        )))
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .tokenSettings(TokenSettings.builder().accessTokenFormat(OAuth2TokenFormat.REFERENCE)
-                        .accessTokenTimeToLive(Duration.ofMinutes(15))
-                        .authorizationCodeTimeToLive(Duration.ofMinutes(2)).build())
-                .redirectUris(uris -> {
-                    uris.addAll(redirectUris);
-                })
-                .scopes(scopes -> scopes.addAll(List.of(
-                        OidcScopes.OPENID, OidcScopes.PROFILE, OidcScopes.EMAIL, "offline_access"
-                )))
-                .clientSettings(ClientSettings.builder().requireProofKey(true).requireAuthorizationConsent(false).build())
-                .build();
-
-        // Save registered client in db as if in-memory
-        JdbcRegisteredClientRepository registeredClientRepository = new JdbcRegisteredClientRepository(jdbcTemplate);
-        registeredClientRepository.save(demoClient);
-        registeredClientRepository.save(demoClientPkce);
-        registeredClientRepository.save(demoClientOpaque);
-        registeredClientRepository.save(demoClientPkceOpaque);
-
-        LOGGER.info("Registered OAuth2/OIDC clients");
-
-        return registeredClientRepository;
-    }
-
-
-    @Bean
-    public OAuth2AuthorizationService authorizationService(JdbcTemplate jdbcTemplate, RegisteredClientRepository registeredClientRepository) {
-        return new JdbcOAuth2AuthorizationService(jdbcTemplate, registeredClientRepository);
-    }
-
-    @Bean
-    public OAuth2AuthorizationConsentService authorizationConsentService(JdbcTemplate jdbcTemplate, RegisteredClientRepository registeredClientRepository) {
-        return new JdbcOAuth2AuthorizationConsentService(jdbcTemplate, registeredClientRepository);
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     /*
